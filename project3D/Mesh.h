f02b27d2acf8eedaf54cee6c5c2a36e8dbee73eb
ThePyromage <the.pyromagi@gmail.com>
2019-04-04 16:39:45 +1100

    mesh class added

---
+++ project3D/Mesh.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class Mesh
+{
+public:
+	Mesh() : triCount(0), vao(0), vbo(0), ibo(0) {}
+	virtual ~Mesh();
+
+	struct Vertex
+	{
+		glm::vec4 position;
+		glm::vec4 normal;
+		glm::vec2 texCoord;
+	};
+
+	void initialiseQuad();
+
+	virtual void draw();
+
+protected:
+	unsigned int triCount;
+	unsigned int vao, vbo, ibo;
+};
+
